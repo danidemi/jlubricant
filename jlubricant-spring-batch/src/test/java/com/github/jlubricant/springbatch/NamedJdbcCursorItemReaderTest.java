ed3badea423a7928a3ef3dd774b086a643205f96
danidemi <demichelis@danidemi.com>
2014-05-17 23:19:17 +0200

    Db moved to right package

--- jlubricant-spring-batch/src/test/java/com/github/jlubricant/springbatch/NamedJdbcCursorItemReaderTest.java
+++ jlubricant-spring-batch/src/test/java/com/github/jlubricant/springbatch/NamedJdbcCursorItemReaderTest.java
@@ -1,81 +1,65 @@
 package com.github.jlubricant.springbatch;
 
-import static org.junit.Assert.*;
-
-import java.util.Map;
-
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.springframework.batch.item.ExecutionContext;
-import org.springframework.jdbc.core.ColumnMapRowMapper;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-
-import com.github.jlubricant.test.HsqlDb;
 
 public class NamedJdbcCursorItemReaderTest {
 	
-	static HsqlDb testDb;
-	
-	@BeforeClass
-	public static void setUpDb() {
-		testDb = new HsqlDb(NamedJdbcCursorItemReaderTest.class);
-		testDb.start();
-	}
-	
-	@Before
-	public void setUpTables() throws Exception {
-		testDb.truncate();
-		testDb
-			.executeBatch(
-					"CREATE TABLE PEOPLE (firstName VARCHAR(64), lastName VARCHAR(64))",
-					"INSERT INTO PEOPLE (firstName, lastName) VALUES ('john', 'doe')",
-					"INSERT INTO PEOPLE (firstName, lastName) VALUES ('susie', 'wong')",
-					"INSERT INTO PEOPLE (firstName, lastName) VALUES ('zon', 'gong')",
-					"CREATE TABLE EMPTY (a_field VARCHAR(32))"
-			);
-	}
-	
-	@AfterClass
-	public static void tearDownDb() {
-		testDb.stop();
-	}
-
-	@Test public void playAround() throws Exception {
-		
-		// given
-		DriverManagerDataSource ds = new DriverManagerDataSource();
-		ds.setDriverClassName(testDb.getDriverClassName());
-		ds.setPassword(testDb.getPassword());
-		ds.setUsername(testDb.getUsername());
-		ds.setUrl(testDb.getConnectionString());
-		
-		
-		NamedJdbcCursorItemReader<Map<String, Object>> reader = new NamedJdbcCursorItemReader<Map<String, Object>>();
-		reader.setDataSource(ds);
-		reader.setSql("SELECT * FROM PEOPLE WHERE firstName = :FIELD");
-		reader.setParamSource( new MapSqlParameterSource("FIELD", "john") );
-		reader.setRowMapper(new ColumnMapRowMapper());
-		reader.afterPropertiesSet();
-		
-		// when
-		reader.open(new ExecutionContext());
-		Map<String, Object> read = reader.read();
-		reader.close();
-		
-		// then
-		assertNotNull( read );
-		assertEquals( "john", read.get("firstName") );
-		assertEquals( "doe", read.get("lastName") );
-		
-	}
+//	HsqlDbms testDb;
+//	
+//	@BeforeClass
+//	public static void setUpDb() {
+//	}
+//	
+//	@Before
+//	public void setUpTables() throws Exception {
+//		HsqlDatabase db = new HsqlDatabase();
+//		db.setDbName("test-db");
+//		db.setStorage(new InProcessInMemory());
+//		testDb = new HsqlDbms();
+//		testDb.add( db );
+//		testDb.start();
+//		db.
+//			.executeBatch(
+//					"CREATE TABLE PEOPLE (firstName VARCHAR(64), lastName VARCHAR(64))",
+//					"INSERT INTO PEOPLE (firstName, lastName) VALUES ('john', 'doe')",
+//					"INSERT INTO PEOPLE (firstName, lastName) VALUES ('susie', 'wong')",
+//					"INSERT INTO PEOPLE (firstName, lastName) VALUES ('zon', 'gong')",
+//					"CREATE TABLE EMPTY (a_field VARCHAR(32))"
+//			);
+//	}
+//	
+//	@AfterClass
+//	public static void tearDownDb() {
+//		testDb.stop();
+//	}
+//
+//	@Test public void playAround() throws Exception {
+//		
+//		// given
+//		DriverManagerDataSource ds = new DriverManagerDataSource();
+//		ds.setDriverClassName(testDb.getDriverClassName());
+//		ds.setPassword(testDb.getPassword());
+//		ds.setUsername(testDb.getUsername());
+//		ds.setUrl(testDb.getConnectionString());
+//		
+//		
+//		NamedJdbcCursorItemReader<Map<String, Object>> reader = new NamedJdbcCursorItemReader<Map<String, Object>>();
+//		reader.setDataSource(ds);
+//		reader.setSql("SELECT * FROM PEOPLE WHERE firstName = :FIELD");
+//		reader.setParamSource( new MapSqlParameterSource("FIELD", "john") );
+//		reader.setRowMapper(new ColumnMapRowMapper());
+//		reader.afterPropertiesSet();
+//		
+//		// when
+//		reader.open(new ExecutionContext());
+//		Map<String, Object> read = reader.read();
+//		reader.close();
+//		
+//		// then
+//		assertNotNull( read );
+//		assertEquals( "john", read.get("firstName") );
+//		assertEquals( "doe", read.get("lastName") );
+//		
+//	}
 	
 }
 
