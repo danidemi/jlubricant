fc4a09ebc6dea3f85efadefdf0fc71df9a81279c
danidemi <demichelis@danidemi.com>
2014-06-02 23:45:11 +0200

    class finder

--- jlubricant-embeddable-h2/src/main/java/com/danidemi/jlubricant/embeddable/h2/H2Storage.java
+++ jlubricant-embeddable-h2/src/main/java/com/danidemi/jlubricant/embeddable/h2/H2Storage.java
@@ -1,5 +1,10 @@
 package com.danidemi.jlubricant.embeddable.h2;
 
+/**
+ * Implementations define how H2 can store database data. 
+ * 
+ * @author Daniele Demichelis
+ */
 public abstract class H2Storage {
 
 	public abstract String getStorageSpecifier();
